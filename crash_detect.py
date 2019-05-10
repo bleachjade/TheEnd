7b536cb8ca39f1e6e14ba8f0df548d5ff34570c1
bleachjade <bleachjade@hotmail.com>
2019-05-10 09:17:14 +0700

    fix crash

--- crash_detect.py
+++ crash_detect.py
@@ -1,5 +1,5 @@
 def check_crash(player_x, player_y, bullet_x, bullet_y):
-    if bullet_x - 23 < player_x < bullet_x + 23 and bullet_y - 25 < player_y < bullet_y + 25:
+    if bullet_x - 32 < player_x < bullet_x + 32 and bullet_y - 33 < player_y < bullet_y + 33:
         return True
     else:
         return False
